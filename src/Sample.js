53addb76e8cf407ecac2ec2bdf6c622edaed6581
Hala Elsaka <helsaka@qe-software.com>
2021-07-29 11:43:07 +0200

    adding save button

--- src/Sample.js
+++ src/Sample.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from "react";
-import { Form, Tabs, Input, Col } from "antd";
+import { Form, Tabs, Input, Col, Row, Button } from "antd";
 import DWT from "./DynamsoftSDK";
 
 const { TabPane } = Tabs;
@@ -9,31 +9,61 @@ const Sample = (props) => {
   const changeKey = (key) => {
     setActiveKey(key);
   };
+  const formItemLayout = {
+    labelCol: { span: 24 },
+    wrapperCol: { span: 20 },
+  };
   return (
     <>
-      <Form>
+      <Form {...formItemLayout}>
         <Tabs activeKey={activeKey} onTabClick={changeKey} style={{ marginLeft: "2rem" }}>
           <TabPane tab="Inputs" key="1">
-            <Col span={8}>
-              <Form.Item
-                label="Title"
-                name="title"
-                rules={[
-                  {
-                    required: true,
-                    message: `Please input Title`,
-                  },
-                ]}
-                style={{ marginTop: "-15px" }}
-              >
-                <Input style={{ borderRadius: "8px" }} />
-              </Form.Item>
-            </Col>
+            <Row>
+              <Col span={8}>
+                <Form.Item
+                  label="Title"
+                  name="title"
+                  rules={[
+                    {
+                      required: true,
+                      message: `Please input Title`,
+                    },
+                  ]}
+                  style={{ marginTop: "-15px" }}
+                >
+                  <Input style={{ borderRadius: "8px" }} />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Name"
+                  name="name"
+                  rules={[
+                    {
+                      required: true,
+                      message: `Please input name`,
+                    },
+                  ]}
+                  style={{ marginTop: "-15px" }}
+                >
+                  <Input style={{ borderRadius: "8px" }} />
+                </Form.Item>
+              </Col>
+            </Row>
           </TabPane>
           <TabPane tab="Scan" key="2">
             <DWT features={["scan", "camera", "load", "save", "upload", "barcode", "ocr", "uploader"]} />
           </TabPane>
         </Tabs>
+        <Row gutter={24}>
+          <Col span={24}>
+            <Form.Item style={{ margin: "1.5rem" }} wrapperCol={{ span: 4 }}>
+              <Button type="primary" ghost={true} htmlType="submit">
+                Save
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
       </Form>
     </>
   );
